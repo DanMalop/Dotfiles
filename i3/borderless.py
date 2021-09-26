997b9010c0dcf7b02b29e2dfad69f649370161b5
DanielManrique60 <dd_180796@hotmail.com>
2021-09-25 22:44:29 -0500

    third commit

---
+++ i3/borderless.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import i3ipc
+
+def on_focus(i3, e):
+    i3.command('[class="^.*"] border pixel 0')
+    i3.command('border pixel 1')
+
+i3 = i3ipc.Connection()
+i3.on('window::focus', on_focus)
+i3.main()
